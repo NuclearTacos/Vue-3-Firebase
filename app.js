ebdc696d2a5949b0ec6508841353fd71237edab5
Christopher Rea <christopher.rea4@gmail.com>
2022-11-27 16:39:38 -0600

    To beginning of CLI section

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
-      title: 'The Final Empire',
-      author: 'Brandon Sanderson',
-      age: 45
+      books: [
+        { title: 'name of the wind', author: 'patrick rothfuss', isFav: true },
+        { title: 'the way of kings', author: 'brandon sanderson', isFav: false },
+        { title: 'the final empire', author: 'brandon sanderson', isFav: true }
+      ]
     }
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav
+    }
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter( (b) => b.isFav == true );
     }
   }
 })
